0c39ebf14dfbd075a588d03d5e80081000bb76e8
ram <ram@htech.ae>
2024-11-26 02:44:08 +0400

    upgrade CSS

--- news-magazine/layout/scripts/galleryviewthemes/jquery.galleryview.setup.js
+++ news-magazine/layout/scripts/galleryviewthemes/jquery.galleryview.setup.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         show_filmstrip: true,           //BOOLEAN - flag to show or hide filmstrip portion of gallery
         
 		panel_width: 600,               //INT - width of gallery panel (in pixels)
-        panel_height: 280,              //INT - height of gallery panel (in pixels)
+        panel_height: 380,              //INT - height of gallery panel (in pixels)
         
 		frame_width: 100,               //INT - width of filmstrip frames (in pixels)
         frame_height: 60,               //INT - width of filmstrip frames (in pixels)
